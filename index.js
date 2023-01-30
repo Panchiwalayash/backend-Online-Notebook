726e58234a09ca55e2b104266bcf52af469809ba
Yash Panchiwala <87870272+Panchiwalayash@users.noreply.github.com>
2023-01-30 14:15:02 +0530

    update

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ require('dotenv').config()
 connectToMongo() ;
 
 const app = express()
+app.use(cors())
 const port = process.env.PORT 
 
-app.use(cors())
 app.use(express.json()) 
 
 app.use('/api/auth',require('./routes/auth'))
